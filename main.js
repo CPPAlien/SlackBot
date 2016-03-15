c5d7654ee39950012e4eb556c973a9b84f138f5b
ChrisPeng <creaspan@gmail.com>
2016-03-15 17:02:25 +0800

    init

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');
 console.log("start ....");
 var ServerListener = function(req, res) {
         res.writeHead(200);
-        res.end('hello world test test');
+        res.end('hello world test test test');
 }
 
 var server = http.createServer(ServerListener);
