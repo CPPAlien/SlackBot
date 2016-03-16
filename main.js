687d70a0e7bea5d2f584f79f9c739c55990a1907
Chris <creaspan@gmail.com>
2016-03-16 10:52:12 +0800

    init

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 var http = require('http');
 var url = require("url");
 var util = require("util");
+var querystring = require('querystring');
 
 console.log("start ....");
 function onRequest(req, res) {
         res.writeHead(200);
-        res.end(util.inspect(url.parse(req.url, true)));
-        console.log(util.inspect(url.parse(req.url, true)));
+
+        var post = ''; 
+    	req.on('data', function(chunk){
+        	post += chunk;
+    	});
+    	req.on('end', function(){
+        	post = querystring.parse(post);
+        	res.end(util.inspect(post));
+        	console.log(util.inspect(post));
+    	});
 }
 
 
